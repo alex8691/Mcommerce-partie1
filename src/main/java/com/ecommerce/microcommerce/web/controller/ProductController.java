eed02df24e67076e618c10dd97298f1d48155d10
alexis maleval <alexis.maleval@gmail.com>
2018-09-11 15:23:40 +0200

    Partie 3 - Validation du prix de vente

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitGratuitException;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
@@ -92,12 +93,15 @@ public class ProductController {
     //ajouter un produit
     @PostMapping(value = "/Produits")
 
-    public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) {
+    public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) throws  ProduitGratuitException{
 
         Product productAdded = productDao.save(product);
 
         if (productAdded == null)
             return ResponseEntity.noContent().build();
+
+        if (productAdded.getPrix() == 0)
+            throw new ProduitGratuitException("Le produit " + productAdded.getNom() + " ne peut être vendu gratuitement.");
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
